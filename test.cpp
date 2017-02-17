27e69454a520f486604769188cf25261eb34873d
magnus <mingran.muc@gmail.com>
2017-02-17 10:45:59 +0800

    validation version

--- test.cpp
+++ test.cpp
@@ -7,23 +7,53 @@ int main() {
   // Read source image.
   Mat im_src = imread("ma.jpg");
 
-  // 4 corners of the book in source image
-  vector<Point2f> pts_dst( {Point2f(220, 54),
-                              Point2f(690, 187),
-                              Point2f(706, 519),
-                              Point2f(207, 432)} );
+  Mat im_src_display = im_src.clone();
+  Point2f nw_point_src(220.f, 54.f);
+  Point2f ne_point_src(690.f, 187.f);
+  Point2f se_point_src(706.f, 519.f);
+  Point2f sw_point_src(207.f, 432.f);
 
-  // Read destination image.
-    // Mat im_dst = imread("ma.jpg");
-    // Four corners of the book in destination image.
-    vector<Point2f> pts_src( {Point2f(0, 0),
-                              Point2f(300, 0),
-                              Point2f(300, 200),
-                              Point2f(0, 200)} );
+  // 4 corners of the book in source image
+  vector<Point2f> pts_src( {nw_point_src,
+                              ne_point_src,
+                              se_point_src,
+                              sw_point_src} );
+
+  
+
+  // set destination image.
+  Point2f nw_point_dst(0.f, 0.f);
+  Point2f ne_point_dst(300.f, 0.f);
+  Point2f se_point_dst(300.f, 200.f);
+  Point2f sw_point_dst(0.f, 200.f);
+  vector<Point2f> pts_dst( {nw_point_dst,
+                              ne_point_dst,
+                              se_point_dst,
+                              sw_point_dst} );
 
   // Calc Homography
   Mat h = findHomography(pts_src, pts_dst);
-  h = h.inv();
+
+  cout << "Homography Mat\'s size is: " ;
+  cout << h.cols << ", " << h.rows << ": " << h.depth() << ": " << h.type() << endl;
+  cout << h << endl << endl;
+
+  float mem_data[3] = {220.f, 54.f, 1.f};
+  Mat src_homogeneous_vector(3, 1, CV_32F, mem_data);
+  cout << "homogeneous vector\'s size is: " ;
+  cout << src_homogeneous_vector.cols << ", " << src_homogeneous_vector.rows << ": " << h.depth() << ": " << h.type() << endl;
+  cout << src_homogeneous_vector << endl << endl;
+
+  // Mat res_mat(3, 1, CV_32F);
+  auto res_mat = h.mul(src_homogeneous_vector);
+  cout << res_mat << endl << endl;
+//  cout << "res mat\'s size is: " ;
+//  cout << res.cols << ", " << res.rows << endl;
+//  cout << res.at<float>(0, 0) << endl;
+//  cout << res.at<float>(1, 0) << endl;
+//  cout << res.at<float>(2, 0) << endl << endl;
+  
+  
 
   // Output image
   Mat im_out;
