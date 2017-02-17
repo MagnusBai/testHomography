c211e5b7b47243879dca4ddfaeddcc6e12400c6f
MagnusBai <mingran.muc@gmail.com>
2017-02-17 09:33:45 +0800

    change image

--- test.cpp
+++ test.cpp
@@ -5,28 +5,29 @@ using namespace cv;
 
 int main() {
   // Read source image.
-  Mat im_src = imread("book2.jpg");
+  Mat im_src = imread("ma.jpg");
 
   // 4 corners of the book in source image
-  vector<Point2f> pts_src( {Point2f(141, 131),
-                              Point2f(480, 159),
-                              Point2f(493, 630),
-                              Point2f(64, 601)} );
+  vector<Point2f> pts_dst( {Point2f(220, 54),
+                              Point2f(690, 187),
+                              Point2f(706, 519),
+                              Point2f(207, 432)} );
 
   // Read destination image.
-    Mat im_dst = imread("book1.jpg");
+    // Mat im_dst = imread("ma.jpg");
     // Four corners of the book in destination image.
-    vector<Point2f> pts_dst( {Point2f(318, 256),
-                              Point2f(534, 372),
-                              Point2f(316, 670),
-                              Point2f(73, 473)} );
+    vector<Point2f> pts_src( {Point2f(0, 0),
+                              Point2f(300, 0),
+                              Point2f(300, 200),
+                              Point2f(0, 200)} );
 
   // Calc Homography
   Mat h = findHomography(pts_src, pts_dst);
+  h = h.inv();
 
   // Output image
   Mat im_out;
-  warpPerspective(im_src, im_out, h, im_dst.size());
+  warpPerspective(im_src, im_out, h, im_src.size());
 
   imwrite("result.png", im_out);
 
